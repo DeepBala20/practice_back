bce14cb2690588eb3513886f2f8a43b1d6f70bd3
Deep_Bala <deepahir3105@gmail.com>
2024-05-11 20:58:21 +0530

    utils code learning

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app} from './app.js'
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(
+    ()=>{
+        app.on("error" ,(err)=>{console.log(err);throw err})
+        app.listen(process.env.PORT || 8000 , ()=>{console.log(`server is running on port : ${process.env.PORT}`);});
+    }
+)
+.catch(
+    (err)=>{
+        console.log("mongodb connection failed !!!" ,err);
+    }
+)
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+class ApiReponse {
+    constructor (statusCode , data , message = "success"){
+        this.statusCode = statusCode
+        this.data = data 
+        this.message = message
+        this.success = statusCode
+    }
+}
+
+export {ApiReponse}
